0cc364b8837e2c84e75ca614042c2c424814f71a
josephchiao <chiaofeng.0118@gmail.com>
2023-01-31 15:57:24 -0800

    changed file directory

---
+++ centennial_robotics_main_file/src/main.py
@@ -0,0 +1,19 @@
+# ---------------------------------------------------------------------------- #
+#                                                                              #
+# 	Module:       main.py                                                      #
+# 	Author:       joseph_chiao                                                 #
+# 	Created:      1/31/2023, 3:55:31 PM                                        #
+# 	Description:  V5 project                                                   #
+#                                                                              #
+# ---------------------------------------------------------------------------- #
+
+# Library imports
+from vex import *
+
+# Brain should be defined by default
+brain=Brain()
+
+brain.screen.print("Hello V5")
+
+
+        

eabeb4afb03b21884387b71f1b2013bee9a85206
snare555 <076-aintihar@sd43.bc.ca>
2023-04-11 16:55:31 -0700

    autoshooting

--- centennial_robotics_main_file/src/shooting_program.py
+++ centennial_robotics_main_file/src/shooting_program.py
@@ -6,8 +6,10 @@ brain=Brain()
 
 # Robot configuration code
 controller_1 = Controller(PRIMARY)
-motor_6 = Motor(Ports.PORT6, GearSetting.RATIO_18_1, False)
 
+motor_1 = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
+
+motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
 
 # wait for rotation sensor to fully initialize
 wait(30, MSEC)
@@ -28,6 +30,25 @@ from vex import *
 # Begin project code
 import math
 
+
+
 while True:
-    motor_6.set_velocity(controller_1.axis2.position()*2)
-    motor_6.spin(FORWARD)
+
+    if motor_1.velocity(RPM) <= -135:
+        motor_2.spin_for(REVERSE, 90, DEGREES)
+        wait(1, SECONDS)
+        motor_2.spin_for(REVERSE, 270, DEGREES)
+
+
+
+
+    if controller_1.buttonA.pressing():
+        motor_1.set_velocity(200)
+        motor_1.spin(REVERSE)
+
+    
+    
+
+    wait(1/60,SECONDS)
+    
+

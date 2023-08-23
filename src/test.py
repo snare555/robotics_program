570e877a2ba933bfdd2b43d33ace03f561dff47b
josephchiao <chiaofeng.0118@gmail.com>
2023-08-23 11:49:20 -0700

    quality of life improvement

--- src/test.py
+++ src/test.py
@@ -20,19 +20,32 @@ controller_1.screen.set_cursor(0, 0)
 controller_1.screen.print("port 1 = green")
 controller_1.screen.new_line()
 controller_1.screen.print("port 2 = red")
+controller_1.screen.new_line()
 
+speed = 50
 
 while True:
+    controller_1.screen.clear_line(3)
+    controller_1.screen.print("Speed: ", speed)
     if controller_1.buttonA.pressing():
-        motor_1.set_velocity(100, PERCENT)
+        motor_1.set_velocity(speed, PERCENT)
         motor_1.spin(FORWARD)
-        motor_2.set_velocity(100, PERCENT)
+        motor_2.set_velocity(speed, PERCENT)
         motor_2.spin(FORWARD)
     elif controller_1.buttonB.pressing():
-        motor_1.set_velocity(-100, PERCENT)
+        motor_1.set_velocity(-speed, PERCENT)
         motor_1.spin(FORWARD)
-        motor_2.set_velocity(-100, PERCENT)
+        motor_2.set_velocity(-speed, PERCENT)
         motor_2.spin(FORWARD)
     else:
         motor_1.stop(COAST)
-        motor_2.stop(COAST)#hello!
+        motor_2.stop(COAST)
+
+    if controller_1.buttonUp.pressing() and speed < 100:
+        speed += 2
+    elif controller_1.buttonDown.pressing() and speed > 0:
+        speed -= 2
+    wait(30, MSEC)
+
+
+
